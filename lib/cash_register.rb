a643b6c9f16f4b2b1ee7220eadcecb8aeef400bc
Alua Baikadamova <abaikad00@gmail.com>
2019-01-10 03:50:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,14 @@
 class CashRegister
-  attr_accessor :age
+  attr_accessor :name, :price
  
   def initialize(age = 0)
     @age = age 
   end 
+ 
+ def add_item (name, price)
+   @name=name
+   @price=price
+ end
  
   def void_last_transaction
   end
